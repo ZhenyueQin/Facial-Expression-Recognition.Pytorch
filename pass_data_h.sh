4b994272d77b104832c77334a7b241ad346ab5a1
Zhenyue Qin <zhenyue.qin@anu.edu.au>
2018-11-12 17:04:24 +1100

    I started trying to make the neural network compose two parts, namely the original face image one and the saliency map one.

--- pass_data_h.sh
+++ pass_data_h.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-scp data/data.h5 liu162@bracewell.hpc.csiro.au:/home/liu162/Project-Guanyin-Eye-Fixation/data
+scp data/CK_data_overlay.h5 liu162@bracewell.hpc.csiro.au:/home/liu162/Project-Guanyin-Eye-Fixation/data
